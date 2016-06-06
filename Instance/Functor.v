079a0ee7aa1cbf09faaf5414f960ef701d64303d
Ricky Elrod <ricky@elrod.me>
2016-06-06 01:25:04 -0400

    The constant functor
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- Instance/Functor.v
+++ Instance/Functor.v
@@ -32,3 +32,11 @@ Proof.
   reflexivity.
 Qed.
 
+Program Definition ConstantFunctor {C D : Category} (a : @ob D) : Functor C D :=
+  {| F_ob := fun _ => a;
+     F_mor := fun _ _ _ => id;
+  |}.
+Next Obligation.
+Proof.
+  rewrite id_left. reflexivity.
+Qed.
