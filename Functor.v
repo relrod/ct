2cbdebb49e28b3091ec2e7b54e5e77af4e0ae34d
Ricky Elrod <ricky@elrod.me>
2016-06-06 00:28:13 -0400

    Functor equivalence
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- Functor.v
+++ Functor.v
@@ -1,7 +1,8 @@
 Require Import CT.Category.
+Require Export Program.
 
 (* F : A -> B *)
-Class Functor (A B : Category) :=
+Record Functor (A B : Category) :=
   {  F_ob : A -> B;
      F_mor : forall {a b}, mor a b -> mor (F_ob a) (F_ob b);
      F_id_law : forall a, F_mor (id a) = id (F_ob a);
@@ -10,6 +11,23 @@ Class Functor (A B : Category) :=
   }.
 
 Arguments F_ob {_ _} _ _.
-Arguments F_mor {_ _} _ _ _ _.
+Arguments F_mor {_ _} _ {_ _} _, {_ _} _ _ _ _.
 Arguments F_id_law {_ _} _ _.
 Arguments F_comp_law {_ _} _ {_ _ _} _ _.
+
+(* Equivalence of functors *)
+Theorem F_eq : forall A B (f1 f2 : Functor A B),
+    @F_ob _ _ f1 = @F_ob _ _ f2 ->
+    @F_mor _ _ f1 ~= @F_mor _ _ f2 ->
+    f1 = f2.
+Proof.
+  intros.
+  destruct f1, f2.
+  simpl in H.
+  simpl in H0.
+  subst.
+  subst.
+  f_equal.
+  apply proof_irrelevance.
+  apply proof_irrelevance.
+Qed.
