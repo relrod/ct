47a4306f02baf1aef5e5eb357cd7cfe8e6c84886
Ricky Elrod <ricky@elrod.me>
2016-06-08 05:07:06 -0400

    Missing Endofunctor here
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- Instance/Functor.v
+++ Instance/Functor.v
@@ -2,6 +2,7 @@ Require Export CT.Instance.Functor.Bifunctor.
 Require Export CT.Instance.Functor.ComposeFunctor.
 Require Export CT.Instance.Functor.ConstantFunctor.
 Require Export CT.Instance.Functor.ContravariantFunctor.
+Require Export CT.Instance.Functor.Endofunctor.
 Require Export CT.Instance.Functor.IdentityFunctor.
 Require Export CT.Instance.Functor.OppositeFunctor.
 Require Export CT.Instance.Functor.Profunctor.
