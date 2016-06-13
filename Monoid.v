4c771edd03d6b14238515257d08ecb3ef4722f85
Ricky Elrod <ricky@elrod.me>
2016-06-12 23:42:26 -0400

    Monoid identity is unique
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- Monoid.v
+++ Monoid.v
@@ -1,5 +1,6 @@
 Require Import CT.Magma.
 Require Import CT.Semigroup.
+Require Import FunctionalExtensionality.
 
 Set Primitive Projections.
 
@@ -9,3 +10,16 @@ Record Monoid {T : Type} :=
     monoid_left_one : forall x, semigroup.(magma).(mu) one x = x;
     monoid_right_one : forall x, semigroup.(magma).(mu) x one = x
   }.
+
+(** Monoid identity is unique. *)
+Theorem monoid_identity_unique {T} {M : @Monoid T} e :
+  (forall x, mu M x e = x) -> e = one M.
+Proof.
+  intros.
+  destruct M.
+  simpl in *.
+  destruct (H e).
+  rewrite <- (H one0).
+  rewrite monoid_left_one0.
+  trivial.
+Qed.
