18edd9b857ca8e3a3b066f8e392c4d71a5a2ff1a
Ricky Elrod <ricky@elrod.me>
2016-06-13 01:57:05 -0400

    group inverse inverse
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- Group.v
+++ Group.v
@@ -24,6 +24,8 @@ Lemma group_unique_unop :
   reflexivity.
 Qed.
 
+(** This is given by [monoid_identity_unique] since a group is a monoid, but we
+    prove it again in a different way, anyway. *)
 Theorem group_identity_unique :
   forall {T} (G : @Group T) e x,
     mu G e x = x -> e = one G.
@@ -35,5 +37,20 @@ Proof.
   assumption.
 Qed.
 
-(* TODO: inverse unique, double inverse, inverse over product, l/r cancellation,
+Theorem group_inverse_inverse :
+  forall {T} (G : @Group T) x,
+    inverse G (inverse G x) = x.
+Proof.
+  intros.
+  transitivity (mu G (inverse G (inverse G x)) (mu G (inverse G x) x)).
+  rewrite gr_inverse_left.
+  rewrite monoid_right_one.
+  reflexivity.
+  rewrite semigroup_assoc.
+  rewrite gr_inverse_left.
+  rewrite monoid_left_one.
+  reflexivity.
+Qed.
+
+(* TODO: inverse unique, inverse over product, l/r cancellation,
    magma/semigroup/monoid/group homomorphisms + laws *)
