c3e81d7e0fd150a34f3cf8ee276efe2fc55f65f0
Ricky Elrod <ricky@elrod.me>
2016-06-15 17:46:50 -0400

    Add script to generate Instance exports
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ scripts/generate-instance-exports.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+pushd CT/Instance
+  for d in `find -type d -print | tail -n +2`; do
+    coqfile="$(echo "$d" | sed 's/^\.\///').v"
+    [ -f "$coqfile" ] && rm "$coqfile"
+    echo "(* This file is auto-generated. *)" > "$coqfile"
+    for f in `find "$d" -name '*.v'`; do
+      module="$(echo "$f" | sed 's/^\.\///' | sed 's/\//./g' | sed 's/\.v$//')"
+      echo "Require Export CT.Instance.$module." >> "$coqfile"
+    done
+    echo "Sorting $coqfile"
+    sort -o "$coqfile" "$coqfile"
+  done
+popd
