5578d79cee5b013d09920ee7bfb2228f56d176b4
Ricky Elrod <ricky@elrod.me>
2016-06-16 03:28:11 -0400

    use Ubuntu Mono for source code in html docs
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- scripts/add_mathjax.sh
+++ scripts/add_mathjax.sh
@@ -3,5 +3,10 @@ pushd html
   sed -i '/<\/title>/a\
 <script type="text/javascript" async\
   src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML">\
-</script>' *.html
+</script>\
+<link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono" rel="stylesheet" type="text/css" />\
+<style type="text/css">\
+  .code { font-family: "Ubuntu Mono", monospace !important; font-size: 100% !important; }\
+</style>\
+' *.html
 popd
