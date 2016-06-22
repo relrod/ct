d04b9e3fed3ad6d4e6aa22155a744aea95bcee26
Ricky Elrod <ricky@elrod.me>
2016-06-22 00:16:07 -0400

    Magma homomorphisms. I think we can define Mag now.
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- CT/Magma.v
+++ CT/Magma.v
@@ -3,3 +3,17 @@ Set Primitive Projections.
 Record Magma {T : Type} :=
   { mu : T -> T -> T
   }.
+
+(* Possibly separate this out at some point. *)
+
+(** * Magma homomorphisms.
+
+Must obey this law:
+\(f (a * b) = f(a) * f(b)\)
+*)
+Record MagmaHomomorphism {A B} (M : @Magma A) (N : @Magma B) :=
+  { magma_hom : A -> B;
+    magma_hom_law :
+      forall (a b : A),
+        magma_hom (mu M a b) = mu N (magma_hom a) (magma_hom b)
+  }.
