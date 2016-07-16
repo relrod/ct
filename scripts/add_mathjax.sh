358b3bf8fc3d48e6cba83564bc586bb0555c3927
Ricky Elrod <ricky@elrod.me>
2016-07-16 01:16:38 -0400

    Use cdnjs for mathjax
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- scripts/add_mathjax.sh
+++ scripts/add_mathjax.sh
@@ -2,7 +2,7 @@
 pushd html
   sed -i '/<\/title>/a\
 <script type="text/javascript" async\
-  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML">\
+  src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.6.1/MathJax.js?config=TeX-MML-AM_CHTML">\
 </script>\
 <link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono" rel="stylesheet" type="text/css" />\
 <style type="text/css">\
