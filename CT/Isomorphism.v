3ead0ce59cc4db6c9211ad465b0fe40251e06e0e
Ricky Elrod <ricky@elrod.me>
2016-07-16 00:51:58 -0400

    Prove iso preserved under composition
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- CT/Isomorphism.v
+++ CT/Isomorphism.v
@@ -5,9 +5,31 @@ Require Import CT.Category.
 The idea is that if a morphism 'f' is an isomorphism, then there exists
 an inverse function such that composing it on either side yields the
 identity morphism. *)
-Record Isomorphism {C : Category} (a b : @ob C) :=
+Record Isomorphism {C : Category} {a b : @ob C} :=
   { to : mor a b;
     from : mor b a;
     inv_left : comp from to = id;
     inv_right : comp to from = id;
   }.
+
+(** The composition of two [Isomorphism]s is again an [Isomorphism]. *)
+Theorem iso_comp_iso
+        {C : Category}
+        {a b c : C}
+        (x : @Isomorphism C a b)
+        (y : @Isomorphism C b c) :
+  @Isomorphism C a c.
+Proof.
+  destruct x, y.
+  exists (comp to0 to1) (comp from1 from0).
+  rewrite assoc.
+  rewrite <- (assoc C from1).
+  rewrite inv_left0.
+  rewrite id_right.
+  assumption.
+  rewrite assoc.
+  rewrite <- (assoc C to0).
+  rewrite inv_right1.
+  rewrite id_right.
+  assumption.
+Defined.
