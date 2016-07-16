0aee0448c6a0adf654531ce6bd3ec2f190f74808
Ricky Elrod <ricky@elrod.me>
2016-07-16 00:57:44 -0400

    Fix the freaking build
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- CT/Isomorphism.v
+++ CT/Isomorphism.v
@@ -5,7 +5,7 @@ Require Import CT.Category.
 The idea is that if a morphism 'f' is an isomorphism, then there exists
 an inverse function such that composing it on either side yields the
 identity morphism. *)
-Record Isomorphism {C : Category} {a b : @ob C} :=
+Record Isomorphism {C : Category} (a b : @ob C) :=
   { to : mor a b;
     from : mor b a;
     inv_left : comp from to = id;
