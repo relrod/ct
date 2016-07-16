20dc2070c95dd8d6857ae4cff7ed897e9fed71ac
Ricky Elrod <ricky@elrod.me>
2016-07-16 17:10:36 -0400

    Move iso_comp_iso to CT.Instance.Isomorphism.
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- CT/Instance/Isomorphism.v
+++ CT/Instance/Isomorphism.v
@@ -16,6 +16,10 @@ Proof.
   rewrite id_right. reflexivity.
 Qed.
 
+(** * Isomorphism is preserved under inverse.
+
+That is, if [Isomorphism a b], then [Isomorphism b a].
+*)
 Program Definition InverseIso
         {C : Category}
         {a b : @ob C}
@@ -34,3 +38,35 @@ Next Obligation.
   simpl.
   assumption.
 Qed.
+
+(** * Isomorphism is preserved under composition.
+
+Let \(C\) be a category and \(a, b, c\) be objects in \(C\). Let
+\(f : a \to b\), and \(g : b \to c\) be isomorphisms in \(C\). Then
+\(g \circ f : a \to c\) is also an isomorphism.
+*)
+Theorem iso_comp_iso
+        {C : Category}
+        {a b c : @ob C}
+        (f : Isomorphism a b)
+        (g : Isomorphism b c) :
+  Isomorphism a c.
+Proof.
+  destruct f, g.
+  exists (comp to to0) (comp from0 from).
+  rewrite assoc.
+  rewrite <- (assoc C from0).
+  rewrite inv_left.
+  rewrite id_right.
+  assumption.
+  rewrite assoc.
+  rewrite <- (assoc C to).
+  rewrite inv_right0.
+  rewrite id_right.
+  assumption.
+Defined.
+
+(** The three above derivations ([IdentityIso], [InverseIso], [iso_comp_iso])
+together form an equivalence relation. At some point we should use
+[Coq.Relations.Relation_Definitions] to make it so.
+*)
--- CT/Isomorphism.v
+++ CT/Isomorphism.v
@@ -18,30 +18,3 @@ Record Isomorphism {C : Category} (a b : @ob C) :=
     inv_left : comp from to = id;
     inv_right : comp to from = id
   }.
-
-(** * Isomorphism is preserved under composition.
-
-Let \(C\) be a category and \(a, b, c\) be objects in \(C\). Let
-\(f : a \to b\), and \(g : b \to c\) be isomorphisms in \(C\). Then
-\(g \circ f : a \to c\) is also an isomorphism.
-*)
-Theorem iso_comp_iso
-        {C : Category}
-        {a b c : @ob C}
-        (f : Isomorphism a b)
-        (g : Isomorphism b c) :
-  Isomorphism a c.
-Proof.
-  destruct f, g.
-  exists (comp to0 to1) (comp from1 from0).
-  rewrite assoc.
-  rewrite <- (assoc C from1).
-  rewrite inv_left0.
-  rewrite id_right.
-  assumption.
-  rewrite assoc.
-  rewrite <- (assoc C to0).
-  rewrite inv_right1.
-  rewrite id_right.
-  assumption.
-Defined.
