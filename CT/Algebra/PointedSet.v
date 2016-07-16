1f88d97e4c82a5d289f5af830ef77e401ec889df
Ricky Elrod <ricky@elrod.me>
2016-07-16 01:38:26 -0400

    Commit old-ish pointed set shenanigans. Not useful yet.
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- CT/Algebra/PointedSet.v
+++ CT/Algebra/PointedSet.v
@@ -14,3 +14,31 @@ Record PointPreservingMap {A B} (X : @PointedSet A) (Y : @PointedSet B) :=
   { point_pres_map : A -> B;
     point_pres_law : point_pres_map (basepoint X) = (basepoint Y)
   }.
+
+
+(* Here's an alternative definition using Sigma types. *)
+Definition PointedSet' := existT (fun basepoint':Type => basepoint').
+
+(* And a proof that the two definitions are isomorphic. *)
+
+Definition NonprimeToPrime {A} (x : @PointedSet A) := PointedSet' A (basepoint x).
+
+Definition PrimeToNonprime (x : {basepoint' : Type & basepoint'}) : @PointedSet (projT1 x) :=
+  {| basepoint := (projT2 x) |}.
+
+Theorem id1 :
+  forall A x,
+    PrimeToNonprime (@NonprimeToPrime A x) = id x.
+Proof.
+  intros.
+  reflexivity.
+Qed.
+
+Theorem id2 :
+  forall x,
+    NonprimeToPrime (PrimeToNonprime x) = id x.
+Proof.
+  intros.
+  destruct x.
+  reflexivity.
+Qed.
