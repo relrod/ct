3fb2725805c0f161fd5cf76945cc9771071cbc8a
Ricky Elrod <ricky@elrod.me>
2016-07-16 01:28:58 -0400

    Might as well document the whole module
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- CT/Isomorphism.v
+++ CT/Isomorphism.v
@@ -1,10 +1,17 @@
 Require Import CT.Category.
 
-(** Isomorphism between objects in a category C.
+(** Isomorphism between objects in a category.
 
-The idea is that if a morphism 'f' is an isomorphism, then there exists
+The idea is that if a morphism \(f\) is an isomorphism, then there exists
 an inverse function such that composing it on either side yields the
-identity morphism. *)
+identity morphism.
+
+That is: Let \(C\) be a category and \(a, b\) be objects in C. Then a morphism
+\(f : a \to b\) is an isomorphism if there exists another morphism
+\(g : b \to a\) such that \(g \circ f = id_a\) and \(f \circ g = id_b\).
+
+In this case, \(a\) and \(b\) are said to be isomorphic to each other.
+ *)
 Record Isomorphism {C : Category} (a b : @ob C) :=
   { to : mor a b;
     from : mor b a;
