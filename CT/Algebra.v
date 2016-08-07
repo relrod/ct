b2384a617d20524a007df5f8c692abed2e459364
Ricky Elrod <ricky@elrod.me>
2016-08-07 12:54:34 -0400

    Make generate-instance-exports work for CT.<stuff>
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ CT/Algebra.v
@@ -0,0 +1,9 @@
+Require Export CT.Algebra.AbelianGroup.
+Require Export CT.Algebra.Group.
+Require Export CT.Algebra.Magma.
+Require Export CT.Algebra.Monoid.
+Require Export CT.Algebra.PointedSet.
+Require Export CT.Algebra.Ring.
+Require Export CT.Algebra.Rng.
+Require Export CT.Algebra.Semigroup.
+(* This file is auto-generated. *)
--- scripts/generate-instance-exports.sh
+++ scripts/generate-instance-exports.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-pushd CT/Instance
+pushd CT
   grep -l '\(\* This file is auto-generated. \*\)' . -R | xargs rm -v
   for d in `find -type d -print | tail -n +2`; do
     coqfile="$(echo "$d" | sed 's/^\.\///').v"
@@ -8,7 +8,7 @@ pushd CT/Instance
     echo "(* This file is auto-generated. *)" > "$coqfile"
     for f in `find "$d" -name '*.v'`; do
       module="$(echo "$f" | sed 's/^\.\///' | sed 's/\//./g' | sed 's/\.v$//')"
-      echo "Require Export CT.Instance.$module." >> "$coqfile"
+      echo "Require Export CT.$module." >> "$coqfile"
     done
     echo "Sorting $coqfile"
     sort -o "$coqfile" "$coqfile"
