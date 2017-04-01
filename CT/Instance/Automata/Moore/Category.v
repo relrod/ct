d7232261a5327db17d7fe6f584587e47e1c8f7ec
Ricky Elrod <ricky@elrod.me>
2017-04-01 07:20:25 -0400

    Fix a missing [Qed], nuke unnecessary proof.
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- CT/Instance/Automata/Moore/Category.v
+++ CT/Instance/Automata/Moore/Category.v
@@ -8,19 +8,12 @@ Program Definition MooreCategory : Category :=
      mor := MooreMorphism;
      comp := fun _ _ _ x y => MooreMorphism_composition x y;
      id := fun _ => MooreMorphism_identity;
-     assoc := fun _ _ _ _ => MooreMorphism_assoc;
-
+     assoc := fun _ _ _ _ => MooreMorphism_assoc
   |}.
 Next Obligation.
-Proof. symmetry. apply MooreMorphism_assoc.
+Proof. symmetry. apply MooreMorphism_assoc. Qed.
 Next Obligation.
 Proof.
-  apply MooreMorphism_eq.
-  simpl. reflexivity.
-  simpl. reflexivity.
-  simpl. reflexivity.
-Qed.
-Next Obligation.
   apply MooreMorphism_eq.
   simpl. reflexivity.
   simpl. reflexivity.
