91d5ecf736ad9bce7e907b72d1f94475d3601b55
Ricky Elrod <ricky@elrod.me>
2017-09-18 15:55:17 -0400

    fix latex generation [ci skip]
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- CT/Algebra/Lattice.v
+++ CT/Algebra/Lattice.v
@@ -138,10 +138,10 @@ function from the carrier set of $A$ to the carrier set of $B$ that preserves
 meet, join, and where required, top and bottom.
 
 We have:
-  - $f(x /\ y) = f(x) /\ f(y)$
-  - $f(\top) = \top$
-  - $f(\bot) = \bot$
-  - $f(x \/ y) = f(x) \/ f(y)$
+  - \(f(x /\ y) = f(x) /\ f(y)\)
+  - \(f(\top) = \top\)
+  - \(f(\bot) = \bot\)
+  - \((x \/ y) = f(x) \/ f(y)\)
 
 A homomorphism with these laws is necessarily monotone, though we don't prove
 that here right now.
