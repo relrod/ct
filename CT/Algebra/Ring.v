d61dc44822fe7fb76435225e6966430d6d176d3c
Rick Elrod <rick@elrod.me>
2018-03-04 15:51:32 -0500

    [ring hom] Composition, proof-irrelevant equality, etc
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- CT/Algebra/Ring.v
+++ CT/Algebra/Ring.v
@@ -1,8 +1,10 @@
+Require Import Coq.Program.Tactics.
 Require Import CT.Algebra.Magma.
 Require Import CT.Algebra.Monoid.
 Require Import CT.Algebra.Semigroup.
 Require Import CT.Algebra.Group.
 Require Import CT.Algebra.AbelianGroup.
+Require Import ProofIrrelevance.
 
 (** * Rings
 
@@ -42,3 +44,80 @@ Record RingHomomorphism {A B} (M : @Ring A) (N : @Ring B) :=
     ring_id_hom_law :
       ring_hom (one (monoid M)) = one (monoid N)
   }.
+
+(** * Composition of maps. *)
+Program Definition ring_hom_composition
+        {T U V : Type}
+        {A : @Ring T}
+        {B : @Ring U}
+        {C : @Ring V}
+        (map1 : RingHomomorphism A B)
+        (map2 : RingHomomorphism B C) :
+  RingHomomorphism A C :=
+  {| ring_hom := fun a => (ring_hom B C map2) ((ring_hom A B map1) a) |}.
+Next Obligation.
+Proof.
+  destruct A, B, C, map1, map2.
+  simpl in *.
+  rewrite ring_monoid_hom_law0.
+  rewrite ring_monoid_hom_law1.
+  trivial.
+Qed.
+Next Obligation.
+Proof.
+  destruct A, B, C, map1, map2.
+  simpl in *.
+  rewrite ring_group_hom_law0.
+  rewrite ring_group_hom_law1.
+  trivial.
+Qed.
+Next Obligation.
+Proof.
+  destruct A, B, C, map1, map2.
+  simpl in *.
+  rewrite ring_id_hom_law0.
+  assumption.
+Qed.
+
+(** * Equality of maps, assuming proof irrelevance. *)
+Theorem ring_hom_eq : forall A B F G (N M : RingHomomorphism F G),
+    @ring_hom A B F G N = @ring_hom A B F G M ->
+    N = M.
+Proof.
+  intros.
+  destruct N, M.
+  simpl in *.
+  subst.
+  f_equal.
+  intros.
+  rewrite H4.
+  rewrite H5.
+  rewrite H6.
+  trivial.
+  apply proof_irrelevance.
+  apply proof_irrelevance.
+  apply proof_irrelevance.
+Qed.
+
+(** * Associativity of composition of maps. *)
+Program Definition ring_hom_composition_assoc
+        {T : Type}
+        {A B C D : @Ring T}
+        (f : RingHomomorphism A B)
+        (g : RingHomomorphism B C)
+        (h : RingHomomorphism C D) :
+  ring_hom_composition f (ring_hom_composition g h) =
+  ring_hom_composition (ring_hom_composition f g) h.
+Proof.
+  destruct f, g, h.
+  apply ring_hom_eq.
+  simpl.
+  reflexivity.
+Qed.
+
+(** * Identity map. *)
+Program Definition ring_hom_id
+        {A : Type}
+        {M : @Ring A} :
+  RingHomomorphism M M :=
+  {| ring_hom := fun a => a |}.
