27e62759c19694b443a94e487c16a94d2520bdfb
Rick Elrod <rick@elrod.me>
2018-03-05 01:51:28 -0500

    Generate exports for new instances
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- CT/Instance.v
+++ CT/Instance.v
@@ -1,5 +1,10 @@
+Require Export CT.Instance.Algebra.Ab.
+Require Export CT.Instance.Algebra.Grp.
 Require Export CT.Instance.Algebra.Lat.
+Require Export CT.Instance.Algebra.Monoid.
 Require Export CT.Instance.Algebra.PointedSetCategory.
+Require Export CT.Instance.Algebra.Ring.
+Require Export CT.Instance.Algebra.Rng.
 Require Export CT.Instance.Automata.Moore.Category.
 Require Export CT.Instance.Category.ArrowCategory.
 Require Export CT.Instance.Category.Cat.
--- CT/Instance/Algebra.v
+++ CT/Instance/Algebra.v
@@ -1,3 +1,8 @@
+Require Export CT.Instance.Algebra.Ab.
+Require Export CT.Instance.Algebra.Grp.
 Require Export CT.Instance.Algebra.Lat.
+Require Export CT.Instance.Algebra.Monoid.
 Require Export CT.Instance.Algebra.PointedSetCategory.
+Require Export CT.Instance.Algebra.Ring.
+Require Export CT.Instance.Algebra.Rng.
 (* This file is auto-generated. *)
