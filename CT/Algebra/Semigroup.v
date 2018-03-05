997df26cc045bebef950fd4b5cc04d568d077103
Rick Elrod <rick@elrod.me>
2018-03-04 22:02:47 -0500

    [Algebra] semigroup homomorphism properties
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- CT/Algebra/Semigroup.v
+++ CT/Algebra/Semigroup.v
@@ -1,4 +1,6 @@
+Require Import Coq.Program.Tactics.
 Require Import CT.Algebra.Magma.
+Require Import ProofIrrelevance.
 
 Set Primitive Projections.
 
@@ -15,3 +17,61 @@ Record Semigroup {T : Type} :=
 *)
 Definition SemigroupHomomorphism {A B} (M : @Semigroup A) (N : @Semigroup B) :=
   @MagmaHomomorphism A B M N.
+
+(** * Composition of maps. *)
+Program Definition semigroup_hom_composition
+        {T U V : Type}
+        {A : @Semigroup T}
+        {B : @Semigroup U}
+        {C : @Semigroup V}
+        (map1 : SemigroupHomomorphism A B)
+        (map2 : SemigroupHomomorphism B C) :
+  SemigroupHomomorphism A C :=
+  {| magma_hom := fun a => (magma_hom B C map2) ((magma_hom A B map1) a) |}.
+Next Obligation.
+Proof.
+  destruct A, B, C, map1, map2.
+  simpl in *.
+  rewrite magma_hom_law.
+  rewrite magma_hom_law0.
+  trivial.
+Qed.
+
+(** * Equality of maps, assuming proof irrelevance. *)
+Theorem semigroup_hom_eq : forall A B F G (N M : SemigroupHomomorphism F G),
+    @magma_hom A B F G N = @magma_hom A B F G M ->
+    N = M.
+Proof.
+  intros.
+  destruct N, M.
+  simpl in *.
+  subst.
+  f_equal.
+  intros.
+  rewrite H4.
+  trivial.
+  apply proof_irrelevance.
+Qed.
+
+(** * Associativity of composition of maps. *)
+Program Definition semigroup_hom_composition_assoc
+        {T : Type}
+        {A B C D : @Semigroup T}
+        (f : SemigroupHomomorphism A B)
+        (g : SemigroupHomomorphism B C)
+        (h : SemigroupHomomorphism C D) :
+  semigroup_hom_composition f (semigroup_hom_composition g h) =
+  semigroup_hom_composition (semigroup_hom_composition f g) h.
+Proof.
+  destruct f, g, h.
+  apply semigroup_hom_eq.
+  simpl.
+  reflexivity.
+Qed.
+
+(** * Identity map. *)
+Program Definition semigroup_hom_id
+        {A : Type}
+        {M : @Semigroup A} :
+  SemigroupHomomorphism M M :=
+  {| magma_hom := fun a => a |}.
