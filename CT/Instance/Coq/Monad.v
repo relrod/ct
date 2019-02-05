cb8ea9b1ea88c69604154b4fa781b6c490f10cb6
Rick Elrod <rick@elrod.me>
2019-02-05 17:01:55 -0500

    And a slightly annoying list monad.
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- CT/Instance/Coq/Monad.v
+++ CT/Instance/Coq/Monad.v
@@ -9,6 +9,76 @@ Require Import CT.Instance.Functor.ComposeFunctor.
 Require Import CT.Instance.Functor.IdentityFunctor.
 Require Import FunctionalExtensionality List.
 
+Section CoqListMonad.
+  Program Definition CoqListMuNT :
+    NaturalTransformation
+      (ComposeFunctor CoqListFunctor CoqListFunctor)
+      CoqListFunctor :=
+    {| nt_components := fun _ b => concat b
+    |}.
+  Proof.
+    Next Obligation.
+      extensionality q.
+      induction q.
+      - simpl. reflexivity.
+      - simpl. rewrite IHq. rewrite <- map_app. reflexivity.
+    Qed.
+    Next Obligation.
+      extensionality q.
+      induction q.
+      - simpl. reflexivity.
+      - simpl. rewrite <- IHq. rewrite <- map_app. reflexivity.
+    Qed.
+
+  Program Definition CoqListEtaNT :
+    NaturalTransformation IdentityFunctor CoqListFunctor :=
+    {| nt_components := fun {A} (x:A) => @cons A x nil
+    |}.
+
+  Program Definition CoqListMonad : @Monad CoqType :=
+    {| T := CoqListFunctor;
+       eta := CoqListEtaNT;
+       mu := CoqListMuNT
+    |}.
+  Proof.
+    Next Obligation.
+      extensionality q.
+      extensionality x.
+      induction x.
+      simpl.
+      reflexivity.
+      simpl.
+      rewrite IHx.
+      rewrite map_app.
+      rewrite concat_app.
+      assert (concat a = concat (map (fun x0 : list q => x0) a)).
+      rewrite map_id.
+      trivial.
+      rewrite H.
+      reflexivity.
+      Qed.
+    Next Obligation.
+      extensionality q.
+      extensionality x.
+      induction x.
+      simpl.
+      reflexivity.
+      simpl.
+      rewrite IHx.
+      reflexivity.
+    Qed.
+    Next Obligation.
+      extensionality q.
+      extensionality x.
+      induction x.
+      simpl.
+      reflexivity.
+      simpl.
+      rewrite IHx.
+      reflexivity.
+    Qed.
+End CoqListMonad.
+
 Section CoqOptionMonad.
   Program Definition CoqOptionMuNT :
     NaturalTransformation
