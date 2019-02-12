10a59c436b0ed890e29d5e855af7cbb404482c4c
Rick Elrod <rick@elrod.me>
2019-02-12 02:30:36 -0500

    Equivalence of categories: reflexivity and symmetry. Axiomatized transitivty for now so DO NOT USE IT!
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- CT/Instance/Category/Equivalence.v
+++ CT/Instance/Category/Equivalence.v
@@ -1,8 +1,10 @@
 Require Import CT.Category.
 Require Import CT.Functor.
 Require Import CT.Isomorphism.
+Require Import CT.Instance.Category.FunctorCategory.
 Require Import CT.Instance.Functor.ComposeFunctor.
 Require Import CT.Instance.Functor.IdentityFunctor.
+Require Import CT.Instance.Isomorphism.
 Require Import CT.Instance.NaturalTransformation.NaturalIsomorphism.
 Require Import CT.Instance.NaturalTransformation.IdentityNaturalTransformation.
 
@@ -12,3 +14,59 @@ Record CategoryEquivalence (C D : Category) :=
     ceq_nat_iso1 : NaturalIsomorphism (ComposeFunctor G F) (@IdentityFunctor D);
     ceq_nat_iso2 : NaturalIsomorphism (ComposeFunctor F G) (@IdentityFunctor C)
   }.
+
+(** Equivalence of categories forms an equivalence relation. *)
+Section CategoryEquivalence.
+  Context {A B C : Category}.
+  Section CategoryEquivalenceReflexivity.
+    Program Definition category_equiv_refl_nat_iso :
+      NaturalIsomorphism (ComposeFunctor (@IdentityFunctor C)
+                                         (@IdentityFunctor C))
+                         (@IdentityFunctor C).
+    Proof.
+      rewrite comp_identity_identity.
+      apply IdentityIso.
+      trivial.
+      trivial.
+    Qed.
+
+    Program Definition CategoryEquivalenceReflexivity : CategoryEquivalence C C :=
+      {| F := @IdentityFunctor C;
+         G := @IdentityFunctor C;
+         ceq_nat_iso1 := category_equiv_refl_nat_iso;
+         ceq_nat_iso2 := category_equiv_refl_nat_iso
+      |}.
+  End CategoryEquivalenceReflexivity.
+
+  Section CategoryEquivalenceSymmetry.
+    Theorem CategoryEquivalenceSymmetry :
+      CategoryEquivalence A B ->
+      CategoryEquivalence B A.
+    Proof.
+      intros.
+      destruct X.
+      exists G0 F0.
+      trivial.
+      trivial.
+    Qed.
+  End CategoryEquivalenceSymmetry.
+
+  Section CategoryEquivalenceTransitivity.
+    Theorem CategoryEquivalenceTransitivity :
+      CategoryEquivalence A B ->
+      CategoryEquivalence B C ->
+      CategoryEquivalence A C.
+    Proof.
+      intros.
+      destruct X, X0.
+      exists (ComposeFunctor F0 F1) (ComposeFunctor G1 G0).
+      replace (ComposeFunctor (ComposeFunctor G1 G0) (ComposeFunctor F0 F1)) with
+              (ComposeFunctor (ComposeFunctor G1 (ComposeFunctor G0 F0)) F1).
+      Focus 2.
+      rewrite functor_comp_assoc.
+      rewrite functor_comp_assoc.
+      trivial.
+      admit. (* UGH! *)
+    Admitted.
+  End CategoryEquivalenceTransitivity.
+End CategoryEquivalence.
